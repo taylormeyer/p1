32a0b83ffbe5941afda78dfc3e31c545aea91048
Taylor Meyer <tam008@g.harvard.edu>
2018-02-06 22:02:00 -0500

    Added index file with image

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Taylor Meyer</title>
+	<meta charset="utf-8">
+
+	<meta name = viewport" content="width=device-width, initial-scale=1>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Taylor Meyer</h1>
+		<img src='/images/Taylor Meyer.png'>
+		<h2>Introduction</h2>
+		<p>
+		Thanks for visiting my page! I was born in Detroit, MI but raised in Raleigh, NC. I went to undergrad at UNC-Chapel Hill and played on the varsity tennis team (if anyone is looking to place some tennis, let me know!). I just finished my MBA at Kenan-Flagler Business School at UNC, and am now anxious to learn more about programming. I am currently working at Hyperspace Ventures where I focus on managing a small development team.
+		</p>
+
+		<h2>Quote of the Day</h2>
+		<blockquote>Anything is possible if you set your mind to it - Mom </blockquote>
+	</div>
+
+	<script type="text/javascript"></script>
+</body>
+</html>
